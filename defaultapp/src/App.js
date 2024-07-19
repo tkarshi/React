0ac7561a553b8c7983f83e137489b5c91d75eb48
tkarshi <tkarshi2000@gmail.com>
2024-07-19 09:49:56 +0530

    AppImage reusable component is created and used

--- defaultapp/src/App.js
+++ defaultapp/src/App.js
@@ -1,9 +1,16 @@
 import "./App.css";
 
+//create component
+
+function AppImage(props) {
+  return (
+    <img style={{ marginLeft: "10px", marginTop: "5px" }} src={props.url} />
+  );
+}
 function App() {
   return (
     <div className="App">
-      <h1>Hi This is the first web app in React!</h1>
+      {/* <h1>Hi This is the first web app in React!</h1>
       <img src="https://placehold.co/400" />
       <img
         style={{ marginLeft: "10px" }}
@@ -12,7 +19,13 @@ function App() {
       <img
         style={{ marginLeft: "10px" }}
         src="https://placehold.co/600x400.png"
-      />
+      /> */}
+
+      {/* Call the Component */}
+
+      <AppImage url="https://placehold.co/600x400.png" />
+      <AppImage url="https://placehold.co/600x400" />
+      <AppImage url="https://placehold.co/600x400/000000/FFF" />
     </div>
   );
 }
