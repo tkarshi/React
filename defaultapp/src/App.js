f7d8161ede452dd22f048a838e8f3b1ff8ccfb11
tkarshi <tkarshi2000@gmail.com>
2024-07-19 09:23:44 +0530

    using placehold.co size, format, text

--- defaultapp/src/App.js
+++ defaultapp/src/App.js
@@ -4,6 +4,15 @@ function App() {
   return (
     <div className="App">
       <h1>Hi This is the first web app in React!</h1>
+      <img src="https://placehold.co/400" />
+      <img
+        style={{ marginLeft: "10px" }}
+        src="https://placehold.co/600x400?text=Hello+World"
+      />
+      <img
+        style={{ marginLeft: "10px" }}
+        src="https://placehold.co/600x400.png"
+      />
     </div>
   );
 }
